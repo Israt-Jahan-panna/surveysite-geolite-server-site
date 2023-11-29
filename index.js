59248e1bf4fdfe6701defa6286d0c6497eda3091
Israt Jahan <isratjahanpanna87@gmail.com>
2023-11-29 19:40:56 +0600

    added

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+var cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const app = express();
+const port = process.env.PORT || 4100
+// midelwire
+app.use(cors({
+  origin: ['https://geolite-client-site.web.app'],
+  credentials:true
+}));
+app.use(express.json());
+
+// mongodb connections 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.j0yhois.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri)
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+    try {
+      // Connect the client to the server	(optional starting in v4.7)
+      // await client.connect();
+      // Send a ping to confirm a successful connection
+      await client.db("admin").command({ ping: 1 });
+      console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+      // Ensures that the client will close when you finish/error
+      // await client.close();
+    }
+  }
+  run().catch(console.dir);
+app.get('/', (req, res) => {
+    res.send('geolitesite')
+  })
+  
+  app.listen(port, () => {
+    console.log(`geolite app listening on port ${port}`)
+  })
